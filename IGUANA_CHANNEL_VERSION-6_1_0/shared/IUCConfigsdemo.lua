36bcdb6cea8dc19e7a0d4df40e50571e4403145c
amfiras <amfiras>
2018-09-21 15:50:14 +0000

    Export: demo export

--- IGUANA_CHANNEL_VERSION-6_1_0/shared/IUCConfigsdemo.lua
+++ IGUANA_CHANNEL_VERSION-6_1_0/shared/IUCConfigsdemo.lua
@@ -1,4 +1,4 @@
-local groupName = 'GROUPNAME'
+local groupName = 'demo'
 local envVariable = 'serverType'..groupName
 local serverType = os.getenv(envVariable)
 
@@ -6,22 +6,22 @@ local iucConf = {}
 
 iucConf.dev = {
    -- Configurations for patient csv File
-   ['patientCsvPath'] = '/home/centos/iuc/GROUPNAME/',
+   ['patientCsvPath'] = '/home/centos/iuc/demo/',
    ['patientCsvFile'] = 'PatientData.txt',
 	
    -- Configurations for patient DB file
-   ['patientDbPath'] = '/home/centos/iuc/GROUPNAME/',
+   ['patientDbPath'] = '/home/centos/iuc/demo/',
    ['patientDbFile'] = 'PatientData.sqlite'
 }
 
 
 iucConf.prod = {
    -- Configurations for patient csv File
-   ['patientCsvPath'] = '/home/centos/iuc/prod/GROUPNAME/',
+   ['patientCsvPath'] = '/home/centos/iuc/prod/demo/',
    ['patientCsvFile'] = 'PatientData.txt',
 	
    -- Configurations for patient DB file
-   ['patientDbPath'] = '/home/centos/iuc/prod/GROUPNAME/',
+   ['patientDbPath'] = '/home/centos/iuc/prod/demo/',
    ['patientDbFile'] = 'PatientData.sqlite'
 }
 
